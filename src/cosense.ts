4fbcf6bb41b01beddc8389179e8ef4ef1dd93de8
yosider <yosider.on.earth@gmail.com>
2024-12-31 00:00:36 +0900

    add @cosense/types, remove old parts

--- src/cosense.ts
+++ src/cosense.ts
@@ -1,42 +1,6 @@
-/** 雑な型を使っている */
+import type { Page } from "@cosense/types/rest";
 
-// /api/pages/:projectname/:pagetitle
-type GetPageResponse = {
-  id: string;
-  title: string;
-  lines: {
-    text: string;
-  }[];
-  links: string[];
-  relatedPages: {
-    links1hop: {
-      title: string;
-      descriptions: string[];
-    }[];
-  };
-};
-
-async function getPage(
-  projectName: string,
-  pageName: string,
-  sid?: string,
-): Promise<GetPageResponse | null> {
-  const response = sid
-    ? await fetch(`https://cosen.se/api/pages/${projectName}/${pageName}`, {
-        headers: { Cookie: `connect.sid=${sid}` },
-      }).catch(() => null)
-    : await fetch(
-        `https://cosen.se/api/pages/${projectName}/${pageName}`,
-      ).catch(() => null);
-
-  if (!response) {
-    return null;
-  }
-  const page = await response.json();
-  return page as GetPageResponse;
-}
-
-function toReadablePage(page: GetPageResponse): {
+function toReadablePage(page: Page): {
   title: string;
   description: string;
 } {
